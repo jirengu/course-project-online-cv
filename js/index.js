8893461714f63d6b9eb39280aa315aa102f220d5
若愚 <hunger@jirengu.com>
2019-06-20 15:16:28 +0800

    iconfont

--- js/index.js
+++ js/index.js
@@ -1,41 +1,30 @@
 
 
 window.onhashchange = () => {
+  console.log('hash')
   document.querySelector('#page').className = location.hash.replace('#', '')
 }
 
 
-
-function throttle(fn, gapTime) {
-  let lastTime = null
-  let nowTime = null
-  return function() {
-      nowTime = Date.now()
-      if(!lastTime || nowTime - lastTime > gapTime) {
-          fn.apply(this, arguments)
-          lastTime = nowTime
-      }
-  }
-}
-
 let index = 0
 let pageTabs = document.querySelectorAll('nav a')
-
-function fn(e) {
-  if(e.deltaY > 0 && index < pageTabs.length - 1 ) {
-    index++
-    pageTabs[index].click()
-  } else if(e.deltaY < 0 && index > 0) {
-    index--
-    pageTabs[index].click()
-  }
-}
-fn = throttle(fn, 800)
-
+let gapTime = 1000 
+let lastTime = null
+let nowTime = null
 
 document.querySelector('#page').onwheel = function(e) {
   e.preventDefault()
-  fn(e)
+  nowTime = Date.now() 
+  if(!lastTime || nowTime - lastTime > gapTime) {
+    lastTime = nowTime
+    if(e.deltaY > 0 && index < pageTabs.length - 1 ) {
+      index++
+      pageTabs[index].click()
+    } else if(e.deltaY < 0 && index > 0) {
+      index--
+      pageTabs[index].click()
+    }
+  }
 }
 
 
